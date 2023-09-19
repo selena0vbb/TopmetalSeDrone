a526c76a162ddcaa5ca2f7de96d027f70e6a9c89
harryxni <harry.x.ni@gmail.com>
2023-09-19 15:32:11 -0700

    automated baseline search

--- scope_control/scope_visa.py
+++ scope_control/scope_visa.py
@@ -2,7 +2,7 @@ import pyvisa
 import matplotlib.pyplot as plt
 import numpy as np
 from struct import unpack
-
+import time
 def to_xy(ADC_data, xyscale):
     volts = (ADC_data-xyscale[0])*xyscale[1]
     time = np.arange(0, xyscale[0] * len(volts), xyscale[0])
@@ -44,20 +44,60 @@ class tek_visa():
         headerlen = 2 + int(data[1])
         header = data[:headerlen]
         ADC_wave_b = data[headerlen:-1]
-        ADC_wave = np.array(unpack('%iB' % len(ADC_wave_b),ADC_wave_b))
-    
-        return ADC_wave, ADC_wave_b 
+        ADC_wave = np.array(unpack('%iB' % len(ADC_wave_b),ADC_wave_b)) 
+        return ADC_wave, ADC_wave_b
+    def get_baseline(self, ch_num=1):
+        #slow method to get the baseline in view
+
+        self.scope.write('DATA:STOP 100')
+        self.scope.write('CURVE?')
+        
+        data=self.scope.read_raw()
+        headerlen = 2 + int(data[1])
+        header = data[:headerlen]
+        ADC_wave_b = data[headerlen:-1]
+        baseline = np.mean(np.array(unpack('%iB' % len(ADC_wave_b),ADC_wave_b))) 
+        offset = float(self.scope.query('CH1:OFFSet?'))
+        print(baseline, offset)
+        while (baseline<=130):
+            offset = offset - 0.01
+            
+            print(offset)
+            self.scope.write('CH1:OFFSet %.2f' %offset)
+            
+            time.sleep(0.9)
+            self.scope.write('CURVE?')
+            
+            data=self.scope.read_raw()
+            headerlen = 2 + int(data[1])
+            header = data[:headerlen]
+            ADC_wave_b = data[headerlen:-1]
+            
+            baseline = np.mean(np.array(unpack('%iB' % len(ADC_wave_b),ADC_wave_b))) 
+
+        while (baseline>=170):
+            offset = offset + 0.01
+            
+            print(offset)
+            self.scope.write('CH1:OFFSet %.2f' %offset)
+            
+            time.sleep(0.9)
+            self.scope.write('DATA:STOP 100')
+            self.scope.write('CURVE?')
+            
+            data=self.scope.read_raw()
+            headerlen = 2 + int(data[1])
+            header = data[:headerlen]
+            ADC_wave_b = data[headerlen:-1]
+            
+            baseline = np.mean(np.array(unpack('%iB' % len(ADC_wave_b),ADC_wave_b))) 
+
 
 if __name__ == '__main__':
     scope = tek_visa();
     scope.get_preamble()
-
-    ADC_wave, b = scope.get_waveform(1)
-    xyscale = scope.get_scale()
-
-    time,volts= to_xy(ADC_wave,xyscale)
-    
-    plt.plot(time, volts)
-    plt.show()
+    scope.get_baseline()
+    #plt.plot(time, volts)
+    #plt.show()
 
 
